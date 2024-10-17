f6fa6fb85919d3f645fda9711661e89ec7bbb257
dming <dmnaac@hotmail.com>
2024-10-17 11:48:33 +0800

    Modified.

--- node_main.cc
+++ node_main.cc
@@ -23,14 +23,6 @@
 #include "gflags/gflags.h"
 #include "tf2_ros/transform_listener.h"
 
-// Template function for getting values of parameters in ROS server
-template<typename Type>
-Type getRosParam(ros::NodeHandle &nodehandle, const std::string &param_name, const Type &default_value){
-  Type param_value;
-  nodehandle.param<Type>(param_name, param_value, default_value);
-  return param_value;
-}
-
 DEFINE_bool(collect_metrics, false,
             "Activates the collection of runtime metrics. If activated, the "
             "metrics can be accessed via a ROS service.");
@@ -56,6 +48,23 @@ DEFINE_string(
 namespace cartographer_ros {
 namespace {
 
+cartographer_ros::Node* node_handle;
+cartographer_ros::TrajectoryOptions* trajectory_options_handle;
+
+void SetInitialPose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg) {
+  //Close current trajectories
+  node_handle->FinishAllTrajectories();
+
+  //Set trajectory builder options with new initial pose
+  *trajectory_options_handle->trajectory_builder_options.mutable_initial_trajectory_pose()->mutable_relative_pose()
+  =cartographer::transform::ToProto(cartographer_ros::ToRigid3d(msg->pose.pose));
+
+  //Start a new trajectory with new initial pose
+  if (FLAGS_start_trajectory_with_default_topics) {
+    node_handle->StartTrajectoryWithDefaultTopics(*trajectory_options_handle);
+  }
+}
+
 void Run() {
   constexpr double kTfBufferCacheTimeInSeconds = 10.;
   tf2_ros::Buffer tf_buffer{::ros::Duration(kTfBufferCacheTimeInSeconds)};
@@ -69,31 +78,12 @@ void Run() {
       cartographer::mapping::CreateMapBuilder(node_options.map_builder_options);
   Node node(node_options, std::move(map_builder), &tf_buffer,
             FLAGS_collect_metrics);
-
-  // Set specified initial pose
-  auto *trajectory_options_handle = &(trajectory_options);
-  ros::NodeHandle nodehandle("~");
-  double translation_x = getRosParam<double>(nodehandle, "/initial_translation_x", 0.0);
-  double translation_y = getRosParam<double>(nodehandle, "/initial_translation_y", 0.0);
-  double translation_z = getRosParam<double>(nodehandle, "/initial_translation_z", 0.0);
-  double rotation_x = getRosParam<double>(nodehandle, "/initial_rotation_x", 0.0);
-  double rotation_y = getRosParam<double>(nodehandle, "/initial_rotation_y", 0.0);
-  double rotation_z = getRosParam<double>(nodehandle, "/initial_rotation_z", 0.0);
-  double rotation_w = getRosParam<double>(nodehandle, "/initial_rotation_w", 1.0);
-  geometry_msgs::Pose init_pose;
-  init_pose.position.x = translation_x;
-  init_pose.position.y = translation_y;
-  init_pose.position.z = translation_z;
-  init_pose.orientation.x = rotation_x;
-  init_pose.orientation.y = rotation_y;
-  init_pose.orientation.z = rotation_z;
-  init_pose.orientation.w = rotation_w;
-  *trajectory_options_handle->trajectory_builder_options.mutable_initial_trajectory_pose()->mutable_relative_pose()
-  =cartographer::transform::ToProto(cartographer_ros::ToRigid3d(init_pose));
-
-
   if (!FLAGS_load_state_filename.empty()) {
     node.LoadState(FLAGS_load_state_filename, FLAGS_load_frozen_state);
+    //Only in pure localization mode, the /initialpose topic is subscribed.
+    trajectory_options_handle = &(trajectory_options);
+    node_handle = &(node);
+    ros::Subscriber initial_pose_sub = node.node_handle()->subscribe("/initialpose", 1, SetInitialPose);
   }
 
   if (FLAGS_start_trajectory_with_default_topics) {
